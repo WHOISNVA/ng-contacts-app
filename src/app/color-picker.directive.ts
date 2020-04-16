7198a6d49291536aed3594f458c93293d16cb36a
nva <kennnaoji@gmail.com>
2020-04-16 14:32:18 -0500

    color picker works

--- src/app/color-picker.directive.ts
+++ src/app/color-picker.directive.ts
@@ -1,17 +1,24 @@
-import { Directive, AfterViewInit, Input, ElementRef } from '@angular/core';
+import { Directive, AfterViewInit, Input, ElementRef, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[myColorPicker]'
 })
-export class ColorPickerDirective implements AfterViewInit {
+export class ColorPickerDirective implements AfterViewInit, OnChanges {
 
   @Input() myColorPicker: string;
 
   constructor(private elRef: ElementRef) { 
   }
 
- ngAfterViewInit(): void {
- this.elRef.nativeElement.style.background = this.myColorPicker;
-  }
+  ngAfterViewInit(): void {
+    this.updateBackground();
+ }
 
+ ngOnChanges(changes: SimpleChanges) {
+  this.updateBackground(); // I am not sure if myColorPicker contains the new value at this point. If not you need to read the new value from the changes object
+ }
+
+ updateBackground() {
+    this.elRef.nativeElement.style.background = this.myColorPicker;
+ }
 }
